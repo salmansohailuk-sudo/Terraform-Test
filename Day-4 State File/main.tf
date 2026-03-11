ce2a85045030ca96202df1423a0bb21cc00719a9
Salman <salmansohailuk@gmail.com>
2026-03-11 02:52:37 +0000

    Directory Structured

--- Day-4 State File/main.tf
+++ Day-4 State File/main.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc" "Dev-VPC" {
+  provider      = aws.devenv
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "Dev-VPC"
--- Day-4 State File/provider.tf
+++ Day-4 State File/provider.tf
@@ -14,5 +14,7 @@ terraform {
 
 
 provider "aws" {
-  
+  alias   = "devenv"
+  region  = "us-east-1"
+  profile = "dev"
 }
